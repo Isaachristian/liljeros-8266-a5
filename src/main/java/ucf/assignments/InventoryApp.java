7856168122f59bcce1cf6d7b6f2f9022902d5814
Isaac Liljeros <isaak.liljeros@gmail.com>
2021-07-22 21:20:00 -0400

    Added UI

---
+++ src/main/java/ucf/assignments/InventoryApp.java
@@ -0,0 +1,23 @@
+package ucf.assignments;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class InventoryApp extends Application {
+
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(ClassLoader.getSystemResource("InventoryApp.fxml"));
+        var scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/InventoryAppStyles.css").toExternalForm());
+        stage.setScene(scene);
+        stage.setMinHeight(400.0);
+        stage.setMinWidth(500.0);
+        stage.show();
+    }
+}
